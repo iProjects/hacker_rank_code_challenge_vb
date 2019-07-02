f37ec888ac0c5a8c480394ad177886305621caa9
kevin mutugi <kevinmk40@gmail.com>
2019-07-03 01:25:15 +0300

    first commit

---
+++ hacker_rank_solve_me_first/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("hacker_rank_solve_me_first")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("hacker_rank_solve_me_first")> 
+<Assembly: AssemblyCopyright("Copyright ©  2019")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("c1ffe41b-db2f-4364-9dae-39ba2b089dcc")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ hacker_rank_solve_me_first/hacker_rank_solve_me_first.vb
@@ -0,0 +1,91 @@
+﻿ 
+Imports System
+
+Module hacker_rank_solve_me_first
+    
+    Public Sub Main()
+        Console.WriteLine("hacker rank in vb <<<<<<<<<<!!!!!!!!!!!!>>>>>>>>>>>")
+        solveMeFirst_function()
+        read_input_and_write_output()
+        data_types_function()
+        sum_arrays_fucntion()
+        Console.ReadLine()
+    End Sub
+
+    Function solveMeFirst(ByVal a As Integer, ByVal b As Integer) As Integer
+        ' Hint: Type Return a+b below 
+        Return a + b
+    End Function
+
+    Sub solveMeFirst_function()
+        Console.WriteLine("enter two number to perform addition....")
+        Dim a As Integer
+        Dim b As Integer
+        Dim sum As Integer
+        a = Console.ReadLine()
+        b = Console.ReadLine()
+        sum = solveMeFirst(a, b)
+        Console.WriteLine("{0} plus {1} equals {2}", a, b, sum)
+    End Sub
+
+    Sub read_input_and_write_output()
+        Console.WriteLine("enter a string literal to see its output....")
+        ' Create a String variable:
+        Dim greeting As String
+
+        ' Read value from stdin and save it to variable:
+        greeting = Console.ReadLine()
+
+        ' Print "Hello, World." to stdout:
+        Console.WriteLine("Hello, World.")
+
+        ' Write your code here; print the contents of the 'greeting' variable to stdout.
+        Console.WriteLine("you entered [ {0} ]", greeting)
+    End Sub
+
+    Sub data_types_function()
+        Dim i As Integer = 4
+        Dim d As Decimal = 4.0
+        Dim s As String = "HackerRank "
+        'Declare second integer, double, and String variables.
+        Dim int1, sum_int As Integer
+        Dim decimal1, sum_decimal As Decimal
+        Dim string1 As String
+        'Read and save an integer, double, and String to your variables.
+        Console.WriteLine("enter one integer to perform addition with {0} ....", i)
+        int1 = Console.ReadLine()
+        Console.WriteLine("enter one decimal to perform addition with {0} ....", d)
+        decimal1 = Console.ReadLine()
+        Console.WriteLine("enter string to perform concatenation....")
+        string1 = Console.ReadLine()
+        'Print the sum of both integer variables on a new line.
+        sum_int = i + int1
+        Console.WriteLine("[ {0} ] plus [ {1} ] equals [ {2} ]", i, int1, sum_int)
+        'Print the sum of the double variables on a new line.
+        sum_decimal = d + decimal1
+        Console.WriteLine("[ {0} ] plus [ {1} ] equals [ {2} ]", d, decimal1, sum_decimal)
+        'Concatenate and print the String variables on a new line
+        'The 's' variable above should be printed first.
+        Console.WriteLine("{0} {1}", s, string1)
+    End Sub
+
+    Sub sum_arrays_fucntion()
+
+    End Sub
+
+End Module
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
